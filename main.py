cf0c5e45bcc889340bcc9e80d92c9162dd27b156
Balázs Cziráky <cziraky@users.noreply.github.com>
2021-04-14 01:26:29 +0200

    Added reaction on magnetic force change

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 init = True
 interacting = False
+asleep = False
+magnetic_focres = (
+    input.magnetic_force(Dimension.X),
+    input.magnetic_force(Dimension.Y),
+    input.magnetic_force(Dimension.Z)
+)
 
 def set_emotion(emotion):
     basic.show_icon(emotion)
 
 def on_startup():
-    global init
+    global init, asleep
     init = True
+    asleep = False
     input.set_sound_threshold(SoundThreshold.LOUD)
     for lb in range(0, 255, 10):
         rub.led_brightness(lb)
@@ -14,15 +21,19 @@ def on_startup():
         basic.pause(10)
     set_emotion(IconNames.HAPPY)
     soundExpression.hello.play()
-    basic.pause(3000)
+    basic.pause(1500)
     rub.set_servo_presets(75, 110, 160)
     rub.position_servo(servoPos.CLOSED, servoSpeed.VERY_FAST)
-    rub.set_led_color(0x00FFFF)
+    for lb in range(0, 255, 10):
+        rub.led_brightness(255 - lb)
+        rub.set_led_color(0x00FFFF)
+        basic.pause(10)
     init = False
 
 def turned_on():
-    global interacting
+    global interacting, asleep
     interacting = True
+    asleep = False
 #    rub.led_brightness(255)
 #    rub.set_led_color(0xFF0000)
     set_emotion(IconNames.CONFUSED)
@@ -32,8 +43,9 @@ def turned_on():
     interacting = False
 
 def turned_off():
-    global interacting
+    global interacting, asleep
     interacting = True
+    asleep = False
 #    rub.led_brightness(255)
 #    rub.set_led_color(0x00FFFF)
     set_emotion(IconNames.ANGRY)
@@ -43,60 +55,101 @@ def turned_off():
     interacting = False
 
 def touched():
-    global interacting
+    global interacting, asleep
     interacting = True
-    rub.led_brightness(255)
-    rub.set_led_color(0xFF0000)
+    asleep = False
+#    rub.led_brightness(255)
+#    rub.set_led_color(0xFF0000)
     set_emotion(IconNames.ANGRY)
     basic.pause(10)
     rub.position_servo(servoPos.OPEN, servoSpeed.FAST)
 #    soundExpression.mysterious.play()
     basic.pause(3000)
-    rub.led_brightness(50)
-    rub.set_led_color(0xFF0000)
-    set_emotion(IconNames.SAD)
     rub.position_servo(servoPos.CLOSED, servoSpeed.MEDIUM)
+#    rub.led_brightness(50)
+#    rub.set_led_color(0xFF0000)
+#    set_emotion(IconNames.SAD)
     basic.pause(200)
     if Math.random_boolean():
-        rub.led_brightness(255)
-        rub.set_led_color(0xFF0000)
-        set_emotion(IconNames.ANGRY)
+#        rub.led_brightness(255)
+#        rub.set_led_color(0xFF0000)
+#        set_emotion(IconNames.ANGRY)
         rub.position_servo(servoPos.OPEN, servoSpeed.FAST)
         basic.pause(1500)
         rub.position_servo(servoPos.CLOSED, servoSpeed.MEDIUM)
+#        rub.led_brightness(50)
+#        rub.set_led_color(0xFF0000)
         basic.pause(200)
 
     interacting = False
 
 def on_sound_loud():
-    global interacting
+    global interacting, asleep
     interacting = True
-    rub.led_brightness(255)
-    rub.set_led_color(0xFFFFFF)
+    asleep = False
+#    rub.led_brightness(255)
+#    rub.set_led_color(0xFFFFFF)
     set_emotion(IconNames.SURPRISED)
     rub.position_servo(servoPos.CLOSED, servoSpeed.VERY_FAST)
 #   soundExpression.sad.play()
     basic.pause(3000)
     interacting = False
 
+def logo_pressed():
+    global asleep
+    asleep = False
+
+def magnetic_force_changed(threshold):
+    temp_magnetic_forces = (
+        input.magnetic_force(Dimension.X),
+        input.magnetic_force(Dimension.Y),
+        input.magnetic_force(Dimension.Z)
+    )
+
+    if Math.max(magnetic_focres[0], temp_magnetic_forces[0]) - Math.min(magnetic_focres[0], temp_magnetic_forces[0]) > threshold:
+        return True
+    elif Math.max(magnetic_focres[1], temp_magnetic_forces[1]) - Math.min(magnetic_focres[1], temp_magnetic_forces[1]) > threshold:
+        return True
+    elif Math.max(magnetic_focres[2], temp_magnetic_forces[2]) - Math.min(magnetic_focres[2], temp_magnetic_forces[2]) > threshold:
+        return True
+    else:
+        return False
+
 rub.on_switch_event(RubEvents.ON, turned_on)
 rub.on_switch_event(RubEvents.OFF, turned_off)
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
 input.on_gesture(Gesture.SHAKE, touched)
+input.on_logo_event(TouchButtonEvent.PRESSED, logo_pressed)
 
 on_startup()
 
 def on_forever():
-    global interacting
+    global interacting, init, asleep, magnetic_focres
+
     if interacting == False and init == False:
-        set_emotion(IconNames.ASLEEP)
-        for lb in range(0, 255, 10):
-            rub.led_brightness(lb)
-            rub.led_rainbow()
-            basic.pause(50)
-        for lb in range(0, 255, 10):
-            rub.led_brightness(255 - lb)
-            rub.led_rainbow()
-            basic.pause(50)
+        if not asleep:
+            for sc in range(3):
+                set_emotion(IconNames.HEART)
+                for lb in range(0, 255, 10):
+                    rub.led_brightness(lb)
+                    rub.led_rainbow()
+                    basic.pause(30)
+                for lb in range(0, 255, 10):
+                    rub.led_brightness(255 - lb)
+                    rub.led_rainbow()
+                    basic.pause(30)
+                asleep = True
+        else:
+            set_emotion(IconNames.ASLEEP)
+            for lb in range(0, 255, 10):
+                magnetic_force_changed(5)
+                rub.led_brightness(lb)
+                rub.led_rainbow()
+                basic.pause(100)
+            for lb in range(0, 255, 10):
+                magnetic_force_changed(5)
+                rub.led_brightness(255 - lb)
+                rub.led_rainbow()
+                basic.pause(100)
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,30 +1,53 @@
 let init = true
 let interacting = false
+let asleep = false
+let magnetic_focres = [input.magneticForce(Dimension.X), input.magneticForce(Dimension.Y), input.magneticForce(Dimension.Z)]
 function set_emotion(emotion: number) {
     basic.showIcon(emotion)
 }
 
 function on_startup() {
+    let lb: number;
     
     init = true
+    asleep = false
     input.setSoundThreshold(SoundThreshold.Loud)
-    for (let lb = 0; lb < 255; lb += 10) {
+    for (lb = 0; lb < 255; lb += 10) {
         rub.ledBrightness(lb)
         rub.setLedColor(0xFFFFFF)
         basic.pause(10)
     }
     set_emotion(IconNames.Happy)
     soundExpression.hello.play()
-    basic.pause(3000)
+    basic.pause(1500)
     rub.setServoPresets(75, 110, 160)
     rub.positionServo(servoPos.Closed, servoSpeed.VeryFast)
-    rub.setLedColor(0x00FFFF)
+    for (lb = 0; lb < 255; lb += 10) {
+        rub.ledBrightness(255 - lb)
+        rub.setLedColor(0x00FFFF)
+        basic.pause(10)
+    }
     init = false
+}
+
+function magnetic_force_changed(threshold: number): boolean {
+    let temp_magnetic_forces = [input.magneticForce(Dimension.X), input.magneticForce(Dimension.Y), input.magneticForce(Dimension.Z)]
+    if (Math.max(magnetic_focres[0], temp_magnetic_forces[0]) - Math.min(magnetic_focres[0], temp_magnetic_forces[0]) > threshold) {
+        return true
+    } else if (Math.max(magnetic_focres[1], temp_magnetic_forces[1]) - Math.min(magnetic_focres[1], temp_magnetic_forces[1]) > threshold) {
+        return true
+    } else if (Math.max(magnetic_focres[2], temp_magnetic_forces[2]) - Math.min(magnetic_focres[2], temp_magnetic_forces[2]) > threshold) {
+        return true
+    } else {
+        return false
+    }
+    
 }
 
 rub.onSwitchEvent(RubEvents.On, function turned_on() {
     
     interacting = true
+    asleep = false
     //     rub.led_brightness(255)
     //     rub.set_led_color(0xFF0000)
     set_emotion(IconNames.Confused)
@@ -36,6 +59,7 @@ rub.onSwitchEvent(RubEvents.On, function turned_on() {
 rub.onSwitchEvent(RubEvents.Off, function turned_off() {
     
     interacting = true
+    asleep = false
     //     rub.led_brightness(255)
     //     rub.set_led_color(0x00FFFF)
     set_emotion(IconNames.Angry)
@@ -47,8 +71,9 @@ rub.onSwitchEvent(RubEvents.Off, function turned_off() {
 input.onSound(DetectedSound.Loud, function on_sound_loud() {
     
     interacting = true
-    rub.ledBrightness(255)
-    rub.setLedColor(0xFFFFFF)
+    asleep = false
+    //     rub.led_brightness(255)
+    //     rub.set_led_color(0xFFFFFF)
     set_emotion(IconNames.Surprised)
     rub.positionServo(servoPos.Closed, servoSpeed.VeryFast)
     //    soundExpression.sad.play()
@@ -58,46 +83,73 @@ input.onSound(DetectedSound.Loud, function on_sound_loud() {
 input.onGesture(Gesture.Shake, function touched() {
     
     interacting = true
-    rub.ledBrightness(255)
-    rub.setLedColor(0xFF0000)
+    asleep = false
+    //     rub.led_brightness(255)
+    //     rub.set_led_color(0xFF0000)
     set_emotion(IconNames.Angry)
     basic.pause(10)
     rub.positionServo(servoPos.Open, servoSpeed.Fast)
     //     soundExpression.mysterious.play()
     basic.pause(3000)
-    rub.ledBrightness(50)
-    rub.setLedColor(0xFF0000)
-    set_emotion(IconNames.Sad)
     rub.positionServo(servoPos.Closed, servoSpeed.Medium)
+    //     rub.led_brightness(50)
+    //     rub.set_led_color(0xFF0000)
+    //     set_emotion(IconNames.SAD)
     basic.pause(200)
     if (Math.randomBoolean()) {
-        rub.ledBrightness(255)
-        rub.setLedColor(0xFF0000)
-        set_emotion(IconNames.Angry)
+        //         rub.led_brightness(255)
+        //         rub.set_led_color(0xFF0000)
+        //         set_emotion(IconNames.ANGRY)
         rub.positionServo(servoPos.Open, servoSpeed.Fast)
         basic.pause(1500)
         rub.positionServo(servoPos.Closed, servoSpeed.Medium)
+        //         rub.led_brightness(50)
+        //         rub.set_led_color(0xFF0000)
         basic.pause(200)
     }
     
     interacting = false
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function logo_pressed() {
+    
+    asleep = false
 })
 on_startup()
 basic.forever(function on_forever() {
     let lb: number;
     
     if (interacting == false && init == false) {
-        set_emotion(IconNames.Asleep)
-        for (lb = 0; lb < 255; lb += 10) {
-            rub.ledBrightness(lb)
-            rub.ledRainbow()
-            basic.pause(50)
+        if (!asleep) {
+            for (let sc = 0; sc < 3; sc++) {
+                set_emotion(IconNames.Heart)
+                for (lb = 0; lb < 255; lb += 10) {
+                    rub.ledBrightness(lb)
+                    rub.ledRainbow()
+                    basic.pause(30)
+                }
+                for (lb = 0; lb < 255; lb += 10) {
+                    rub.ledBrightness(255 - lb)
+                    rub.ledRainbow()
+                    basic.pause(30)
+                }
+                asleep = true
+            }
+        } else {
+            set_emotion(IconNames.Asleep)
+            for (lb = 0; lb < 255; lb += 10) {
+                magnetic_force_changed(5)
+                rub.ledBrightness(lb)
+                rub.ledRainbow()
+                basic.pause(100)
+            }
+            for (lb = 0; lb < 255; lb += 10) {
+                magnetic_force_changed(5)
+                rub.ledBrightness(255 - lb)
+                rub.ledRainbow()
+                basic.pause(100)
+            }
         }
-        for (lb = 0; lb < 255; lb += 10) {
-            rub.ledBrightness(255 - lb)
-            rub.ledRainbow()
-            basic.pause(50)
-        }
+        
     }
     
 })
