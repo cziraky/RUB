fd20b4b6e1ba16963b83d7779143caf31b3a7500
Balázs Cziráky <cziraky@users.noreply.github.com>
2021-04-13 19:07:46 +0200

    Some changes

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
+play_sound_expressions = {
+    'hello': soundExpression.hello.play(),
+    'slide': soundExpression.slide.play(),
+    'giggle': soundExpression.giggle.play(),
+    'soaring': soundExpression.soaring.play(),
+    'sad': soundExpression.sad.play()
+}
+
+def set_emotion(emotion):
+    basic.show_icon(emotion)
+
+def play_sound(expression):
+    play_sound_expressions['hello']
+
+def on_startup():
+    set_emotion(IconNames.HAPPY)
+    play_sound('hello')
+    rub.led_brightness(0)
+    rub.set_led_color(0xFFFFFF)
+    for lb in range(255):
+        rub.led_brightness(lb)
+        rub.led_rainbow()
+        basic.pause(20)
+    play_sound('giigle')
+    rub.set_servo_presets(75, 110, 160)
+    rub.position_servo(servoPos.CLOSED, servoSpeed.VERY_FAST)
+    rub.set_led_color(0x00FFFF)
+
 def my_function():
-    rub.position_servo(servoPos.SWITCHED, servoSpeed.VERY_SLOW)
+    rub.set_led_color(0xFF0000)
+    rub.position_servo(servoPos.SWITCHED, servoSpeed.VERY_FAST)
 rub.on_switch_event(RubEvents.ON, my_function)
 
 def my_function2():
-    rub.position_servo(servoPos.CLOSED, servoSpeed.VERY_SLOW)
+    rub.position_servo(servoPos.CLOSED, servoSpeed.FAST)
+    basic.pause(300)
+    rub.set_led_color(0x00FFFF)
 rub.on_switch_event(RubEvents.OFF, my_function2)
 
-rub.position_servo(servoPos.CLOSED, servoSpeed.VERY_FAST)
+def rainbow_led():
+    for index in range(255):
+        for index2 in range(255):
+            for index3 in range(255):
+                rub.set_led_color(0xff0000)
+led_colors = [0, 16711680, 65280, 255, 16711680, 65280]
+on_startup()
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,16 +1,56 @@
-rub.onSwitchEvent(RubEvents.On, function () {
+let play_sound_expressions = {
+    "hello" : soundExpression.hello.play(),
+    "slide" : soundExpression.slide.play(),
+    "giggle" : soundExpression.giggle.play(),
+    "soaring" : soundExpression.soaring.play(),
+    "sad" : soundExpression.sad.play(),
+}
+
+function set_emotion(emotion: number) {
+    basic.showIcon(emotion)
+}
+
+function play_sound(expression: string) {
+    play_sound_expressions["hello"]
+}
+
+function on_startup() {
+    set_emotion(IconNames.Happy)
+    play_sound("hello")
+    rub.ledBrightness(0)
+    rub.setLedColor(0xFFFFFF)
+    for (let lb = 0; lb < 255; lb++) {
+        rub.ledBrightness(lb)
+        rub.ledRainbow()
+        basic.pause(20)
+    }
+    play_sound("giigle")
+    rub.setServoPresets(75, 110, 160)
+    rub.positionServo(servoPos.Closed, servoSpeed.VeryFast)
+    rub.setLedColor(0x00FFFF)
+}
+
+rub.onSwitchEvent(RubEvents.On, function my_function() {
     rub.setLedColor(0xFF0000)
     rub.positionServo(servoPos.Switched, servoSpeed.VeryFast)
 })
-rub.onSwitchEvent(RubEvents.Off, function () {
+rub.onSwitchEvent(RubEvents.Off, function my_function2() {
     rub.positionServo(servoPos.Closed, servoSpeed.Fast)
     basic.pause(300)
     rub.setLedColor(0x00FFFF)
 })
-rub.setLedColor(0xFFFFFF)
-rub.setServoPresets(75, 110, 160)
-rub.positionServo(servoPos.Closed, servoSpeed.VeryFast)
-rub.setLedColor(0x00FFFF)
-basic.forever(function () {
-	
+function rainbow_led() {
+    for (let index = 0; index < 255; index++) {
+        for (let index2 = 0; index2 < 255; index2++) {
+            for (let index3 = 0; index3 < 255; index3++) {
+                rub.setLedColor(0xff0000)
+            }
+        }
+    }
+}
+
+let led_colors = [0, 16711680, 65280, 255, 16711680, 65280]
+on_startup()
+basic.forever(function on_forever() {
+    
 })
